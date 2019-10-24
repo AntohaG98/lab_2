419b89e84f3d59c0c2b32e049a11ecec1638b37c
AntohaG98 <43339796+AntohaG98@users.noreply.github.com>
2019-10-24 17:53:48 +0300

    Update ex_5.py

--- ex_5.py
+++ ex_5.py
@@ -3,3 +3,17 @@ from librip.ctxmngrs import timer
 
 with timer():
     sleep(5.5)
+
+    from time import sleep
+from librip.ctxmngrs import timer
+
+
+# with timer():
+#    sleep(5.5)
+
+@timer
+def	good():
+	import time
+	time.sleep(5.5)
+
+good()
